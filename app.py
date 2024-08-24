04e48ca971b022e03cbe8c4036a14f2421d391ce
Rahul Somutte <162716180+SomutteR@users.noreply.github.com>
2024-08-24 16:25:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask, jsonify, request, render_template
 import json
-
 app = Flask(__name__)
+# Simulated in-memory storage for data
+data_storage = {
+    "datetime": "",
+    "temperature": 0.0,
+    "x": 0.0,
+    "y": 0.0,
+    "z": 0.0
+}
