f39ff0820da661c26df58caafb11b0c74d61c399
Rahul Somutte <162716180+SomutteR@users.noreply.github.com>
2024-08-24 16:36:26 +0100

    Update test.ino

--- test.ino
+++ test.ino
@@ -8,3 +8,22 @@
 // Replace with your Wi-Fi credentials
 const char* ssid = "rahulA15";
 const char* password = "123456789";
+// Replace with your Flask server's IP address and port
+const char* serverName = "http://192.168.0.94:5007/receive_data";
+
+// Create instances for sensors
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified();
+RTC_DS3231 rtc;
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  // Initialize I2C
+  Wire.begin();
+
+  // Initialize ADXL345 sensor
+  if (!accel.begin()) {
+    Serial.println("Couldn't find ADXL345");
+    while (1);
+  }
