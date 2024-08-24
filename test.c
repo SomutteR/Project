e61b0358d932f61dcb7fc7ba43f9138114a4cc7b
Rahul Somutte <162716180+SomutteR@users.noreply.github.com>
2024-08-24 16:30:42 +0100

    Create test.ino

---
+++ test.c
@@ -0,0 +1,10 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
+#include <RTClib.h> // Library for DS3231 RTC
+
+// Replace with your Wi-Fi credentials
+const char* ssid = "rahulA15";
+const char* password = "123456789";
