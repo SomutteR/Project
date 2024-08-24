5917e14e8c62593f85d75e7ef8c101780235c4d7
Rahul Somutte <162716180+SomutteR@users.noreply.github.com>
2024-08-24 16:27:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,23 @@ data_storage = {
     "y": 0.0,
     "z": 0.0
 }
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/receive_data', methods=['GET'])
+def receive_data():
+    global data_storage
+    x = request.args.get('x', default=0.0, type=float)
+    y = request.args.get('y', default=0.0, type=float)
+    z = request.args.get('z', default=0.0, type=float)
+    datetime = request.args.get('datetime', default="", type=str)
+    temperature = request.args.get('temperature', default=0.0, type=float)
+    
+    data_storage = {
+        "datetime": datetime,
+        "temperature": temperature,
+        "x": x,
+        "y": y,
+        "z": z
+    }
